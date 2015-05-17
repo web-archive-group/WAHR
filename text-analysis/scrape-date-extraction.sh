84a6f874b081da163ac23233a3da67b83d7fd730
ianmilligan1 <i2milligan@uwaterloo.ca>
2015-05-17 15:27:09 -0400

    minor tweaks

--- text-analysis/scrape-date-extraction.sh
+++ text-analysis/scrape-date-extraction.sh
@@ -1,7 +1,19 @@
-# sort of a hackish way to combine hadoop part output and count number 
-# of unique scrapes that it's finding in each one.
+# this first command takes all hadoop output and combines them. Avoids
+# argument list being too long.
 
 printf '%s\0' part* | xargs -0 cat > output.txt
+
+# this following command lets you see the unique scrapes that are
+# within the collection
+
 command | cut -c1-6 merged.txt | sort | uniq
 
-# will generate list of scrapes.
+# this following command sorts the merged data by date-scrape.
+
+sort -n merged.txt > sorted-merged.txt
+
+# if you want to verify, you can run this command to see a scrolling
+# output of the dates in the collection by order. Should not be
+# necessary.
+
+awk '{print $1}' sorted-merged.txt
