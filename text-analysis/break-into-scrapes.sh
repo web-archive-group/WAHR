1c5c6b3a65d3c686c7f1f8330f77d5bffc8dbc52
ianmilligan1 <ianmilligan1@gmail.com>
2015-05-19 16:35:49 -0400

    added descriptive header

--- text-analysis/break-into-scrapes.sh
+++ text-analysis/break-into-scrapes.sh
@@ -1,3 +1,8 @@
+# This is sort of a hacky script that combines all the hadoop part outputs,
+# finds a list of all the unique year/month of scrapes, and then extracts
+# them into text files matching each scrape (i.e. 200510.txt for the scrape 
+# of October 2005. Decent performance as a first attempt?
+
 printf '%s\0' part* | xargs -0 cat > output.txt
 command | cut -c1-6 output.txt | sort | uniq > scrape-dates.txt
 while read p; do
