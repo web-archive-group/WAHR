1000c626673cf7a5f9c4badff6b76bec08e4456c
ianmilligan1 <i2milligan@uwaterloo.ca>
2015-05-20 08:52:54 -0400

    ner scripts

---
+++ text-analysis/ner-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# ner-on-extract.sh
+# run in directory w/ date-scrape text
+# to do: remove intermediate files or pipeline?
+# adapted from
+
+for f in *.txt 
+do
+	/cliphomes/i2millig/stanford-ner/ner.sh $f > $f_ner.txt # creates individualized ner output files
+	sed 's/\/O / /g' < $f_ner.txt > $f_ner_clean.txt # cleaner version of the file, need to get rid of the /O tags for those words that are NOT organization person location
+	egrep -o -f personpattr $f_ner_clean.txt > $f_ner_pers.txt # extracting people
+	cat $f_ner_pers.txt | sed 's/\/PERSON//g' | sort | uniq -c | sort -nr > $f_ner_pers_freq.txt # sorting people frequency
+	egrep -o -f orgpattr $f_ner_clean.txt > $f_ner_org.txt #extracting organization
+	cat $f_ner_org.txt | sed 's/\/ORGANIZATION//g' | sort | uniq -c | sort -nr > $f_ner_org_freq.txt # sorting organization frequency
+	egrep -o -f locpattr $f_ner_clean.txt > $f_ner_loc.txt # extracting locations
+	cat $f_ner_loc.txt | sed 's/\/LOCATION//g' | sort | uniq -c | sort -nr > $f_ner_loc_freq.txt # sorting location frequency
+done
