4911c4dd9d1399a6a0479cc7f5419654f3e22e04
ianmilligan1 <i2milligan@uwaterloo.ca>
2015-06-01 14:03:40 -0400

    solr shine on script

---
+++ scripts/shine/execute.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# run in same directory as solr-execute-on.sh and shine-execute-on.sh
+# give them executable, i.e. chmod +x solr-execute-on.sh
+
+open -a Terminal.app solr-execute-on.sh
+open -a Terminal.app shine-execute-on.sh
---
+++ scripts/shine/shine-execute-on.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /Users/ianmilligan1/desktop/local-shine/shine/shine/
+activator run
