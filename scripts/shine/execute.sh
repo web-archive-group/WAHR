907dd658ffc57aa4d9bd1bc4320d155a245e0982
ianmilligan1 <i2milligan@uwaterloo.ca>
2015-06-14 07:36:09 -0400

    adding postgresql init script

--- scripts/shine/execute.sh
+++ scripts/shine/execute.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # run in same directory as solr-execute-on.sh and shine-execute-on.sh
 # give them executable, i.e. chmod +x solr-execute-on.sh
+# basically, launches postgres in one; solr in another; and shine in the last one
 
+open -a Terminal.app server.sh
 open -a Terminal.app solr-execute-on.sh
 open -a Terminal.app shine-execute-on.sh
---
+++ scripts/shine/server.sh
@@ -0,0 +1,1 @@
+postgres -D /usr/local/var/postgres
