25d09aa9cf4f3a99e9b37be4b3788afc94590b8c
nruest <ruestn@gmail.com>
2015-12-31 10:37:35 -0500

    Let's make this multithreaded. Pool size of three seems to be ok with IA CDX API.

--- scripts/ia_cdx_checker/ia_cdx_checker.py
+++ scripts/ia_cdx_checker/ia_cdx_checker.py
@@ -8,11 +8,20 @@ from __future__ import print_function
 import sys
 import json
 import fileinput
+import logging
 import io
 from urllib2 import Request, urlopen, URLError, HTTPError
+import multiprocessing
 
-for line in fileinput.input():
+POOL_SIZE = 3
+
+logging.basicConfig(filename="ia_cdx.log", level=logging.INFO)
+
+def check_ia_cdx(line):
     elx42_url = line.rstrip('\n')
+    e = None
+    d = None
+    f = None
     try:
         url = 'http://web.archive.org/cdx/search/cdx?url=' + elx42_url + '&output=json&limit=-2'
         request = Request(url, headers={'User-Agent': "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30)"})
@@ -41,8 +50,18 @@ for line in fileinput.input():
         if second_date.startswith('201512'):
             print(elx42_url)
     except HTTPError as e:
-        status_code = e.code
+        logging.error("http error: %s when looking up %s", e, elx42_url)
     except IndexError as d:
-        index_error = d
+        logging.error("index error: %s when looking up %s", d, elx42_url)
     except ValueError as f:
-        value_error = f
+        logging.error("value error: %s when looking up %s", f, elx42_url)
+    #except BadStatusLine as g:
+    #    bad_status_line = g
+
+def main():
+    pool = multiprocessing.Pool(POOL_SIZE)
+    for line in pool.imap_unordered(check_ia_cdx, fileinput.input()):
+        this_is_bad = line
+
+if __name__ == "__main__":
+    main()
