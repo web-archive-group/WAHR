f9d8ec6104025a719ee2e4c6e9cf42cb5e2d94ce
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-03-21 19:05:22 -0400

    revised w/ new code

--- code/Warcbase/Graphx.scala
+++ code/Warcbase/Graphx.scala
@@ -3,4 +3,6 @@ import org.warcbase.spark.matchbox.RecordLoader
 import org.warcbase.spark.matchbox.ExtractGraph
 
 val recs = RecordLoader.loadArchives("/mnt/vol1/data_sets/cpp/cpp_warcs_accession_02/ARCHIVEIT-227-QUARTERLY-JOB182047-20151107015024751-00004.warc.gz", sc) 
-ExtractGraph(recs, "nodes", "links")
+
+val graph = ExtractGraph(recs)
+graph.writeAsJson("nodes", "links")
