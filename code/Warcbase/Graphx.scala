a34444cda2cd380a1bb98bc81c6e9b8a8525e9dc
ianmilligan1 <i2milligan@uwaterloo.ca>
2016-03-21 23:20:12 -0400

    script to run on all cpp

--- code/Warcbase/Graphx.scala
+++ code/Warcbase/Graphx.scala
@@ -2,7 +2,7 @@ import org.warcbase.spark.rdd.RecordRDD._
 import org.warcbase.spark.matchbox.RecordLoader
 import org.warcbase.spark.matchbox.ExtractGraph
 
-val recs = RecordLoader.loadArchives("/mnt/vol1/data_sets/cpp/cpp_warcs_accession_02/ARCHIVEIT-227-QUARTERLY-JOB182047-20151107015024751-00004.warc.gz", sc) 
+val recs = RecordLoader.loadArchives("/mnt/vol1/data_sets/cpp/cpp_warcs_accession_01/partner.archive-it.org/cgi-bin/getarcs.pl/*", sc) 
 
 val graph = ExtractGraph(recs)
-graph.writeAsJson("nodes", "links")
+graph.writeAsJson("nodes-all-CPP-accession-1", "links-all-CPP-accession-1")
